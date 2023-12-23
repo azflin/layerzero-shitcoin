8ceea2a939fd8afc65948730c2584e1144581bef
Autistic Dev <temp@gmail.com>
2023-12-23 15:54:27 +0800

    continue

---
+++ contracts/LayerZeroShitcoin.sol
@@ -0,0 +1,48 @@
+// SPDX-License-Identifier: UNLICENSED
+import { OFTCore, IOFTCore } from "@layerzerolabs/solidity-examples/contracts/token/oft/v1/OFTCore.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/utils/introspection/IERC165.sol";
+import "@layerzerolabs/solidity-examples/contracts/token/oft/v1/interfaces/IOFT.sol";
+
+pragma solidity ^0.8.0;
+
+contract LayerZeroShitcoin is OFTCore, ERC20, IOFT {
+    constructor(
+        string memory _name,
+        string memory _symbol,
+        address _lzEndpoint
+    ) ERC20(_name, _symbol) OFTCore(_lzEndpoint) {}
+
+    function supportsInterface(bytes4 interfaceId) public view virtual override(OFTCore, IERC165) returns (bool) {
+        return interfaceId == type(IOFT).interfaceId || interfaceId == type(IERC20).interfaceId || super.supportsInterface(interfaceId);
+    }
+
+    function token() public view virtual override returns (address) {
+        return address(this);
+    }
+
+    function circulatingSupply() public view virtual override returns (uint) {
+        return totalSupply();
+    }
+
+    function _debitFrom(
+        address _from,
+        uint16,
+        bytes memory,
+        uint _amount
+    ) internal virtual override returns (uint) {
+        address spender = _msgSender();
+        if (_from != spender) _spendAllowance(_from, spender, _amount);
+        _burn(_from, _amount);
+        return _amount;
+    }
+
+    function _creditTo(
+        uint16,
+        address _toAddress,
+        uint _amount
+    ) internal virtual override returns (uint) {
+        _mint(_toAddress, _amount);
+        return _amount;
+    }
+}
